e95fb5c1c6d7a0937c926ae030a252c5a19cf1b4
Tom Matthews <trcm@pm.me>
2021-01-23 12:56:56 +0000

    apply black

--- dumplse.py
+++ dumplse.py
@@ -12,57 +12,36 @@ if len(sys.argv) > 2:
 else:
     POSTS_MAX = None
 
-URL = 'https://www.lse.co.uk/profiles/' + user
+URL = "https://www.lse.co.uk/profiles/" + user
 
 # Maximum chat pages to dump
 GET_MAX = 50
 
-NEXT_PAGE = {
-    'tag': 'a',
-    'class': 'pager__link pager__link--next'
-    }
+NEXT_PAGE = {"tag": "a", "class": "pager__link pager__link--next"}
 
-LAST_PAGE = {
-    'tag': 'a',
-    'class': 'pager__link pager__link--next pager__link--disabled'
-    }
+LAST_PAGE = {"tag": "a", "class": "pager__link pager__link--next pager__link--disabled"}
 
 MSG = {
-    'class': 'share-chat-message__message-content',
-    'name': {
-            'tag': 'p',
-            'class': 'share-chat-message__details--username'
-        },
-    'details': {
-            'tag': 'p',
-            'class': 'share-chat-message__details'
-        },
-    'title': {
-            'tag': 'div',
-            'class': 'share-chat-message__status-bar'
-        },
-    'date': {
-            'tag': 'span',
-            'class': 'share-chat-message__status-bar-time'
-        },
-    'text': {
-            'tag': 'p',
-            'class': 'share-chat-message__message-text'
-        },
-    }
+    "class": "share-chat-message__message-content",
+    "name": {"tag": "p", "class": "share-chat-message__details--username"},
+    "details": {"tag": "p", "class": "share-chat-message__details"},
+    "title": {"tag": "div", "class": "share-chat-message__status-bar"},
+    "date": {"tag": "span", "class": "share-chat-message__status-bar-time"},
+    "text": {"tag": "p", "class": "share-chat-message__message-text"},
+}
 
 posts_retrieved = 0
 for page_num in range(1, GET_MAX):
     try:
-        page = requests.get(URL + '/?page=' + str(page_num))
+        page = requests.get(URL + "/?page=" + str(page_num))
     except requests.exceptions.RequestException as get_error:
         print(f"{Fore.RED}[!] Error: {get_error}{Fore.RESET}")
         sys.exit(1)
 
-    soup = BeautifulSoup(page.content, 'html.parser')
-    post_elems = soup.find_all(class_=MSG['class'])
-    next_page_elem = soup.find(NEXT_PAGE['tag'], class_=NEXT_PAGE['class'])
-    last_page_elem = soup.find(LAST_PAGE['tag'], class_=LAST_PAGE['class'])
+    soup = BeautifulSoup(page.content, "html.parser")
+    post_elems = soup.find_all(class_=MSG["class"])
+    next_page_elem = soup.find(NEXT_PAGE["tag"], class_=NEXT_PAGE["class"])
+    last_page_elem = soup.find(LAST_PAGE["tag"], class_=LAST_PAGE["class"])
 
     if len(post_elems) == 0:
         print(f"{Fore.RED}[!] Nothing found{Fore.RESET}")
@@ -73,13 +52,17 @@ for page_num in range(1, GET_MAX):
             if posts_retrieved >= POSTS_MAX:
                 sys.exit(0)
 
-        name_elem = post.find(MSG['name']['tag'], class_=MSG['name']['class'])
+        name_elem = post.find(MSG["name"]["tag"], class_=MSG["name"]["class"])
         # No good class to find this with, but usually found at index 1 :
-        share_elem = post.find_all(MSG['details']['tag'], class_=MSG['details']['class'])[1]
-        price_elem = post.find_all(MSG['details']['tag'], class_=MSG['details']['class'])[3]
-        title_elem = post.find(MSG['title']['tag'], class_=MSG['title']['class'])
-        date_elem = post.find(MSG['date']['tag'], class_=MSG['date']['class'])
-        text_elem = post.find(MSG['text']['tag'], class_=MSG['text']['class'])
+        share_elem = post.find_all(
+            MSG["details"]["tag"], class_=MSG["details"]["class"]
+        )[1]
+        price_elem = post.find_all(
+            MSG["details"]["tag"], class_=MSG["details"]["class"]
+        )[3]
+        title_elem = post.find(MSG["title"]["tag"], class_=MSG["title"]["class"])
+        date_elem = post.find(MSG["date"]["tag"], class_=MSG["date"]["class"])
+        text_elem = post.find(MSG["text"]["tag"], class_=MSG["text"]["class"])
 
         try:
             text_elem.br.replace_with("\n")
@@ -87,10 +70,12 @@ for page_num in range(1, GET_MAX):
             pass
 
         # Fore: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET
-        print(f'{(Fore.GREEN + name_elem.text + Fore.BLUE + " [" + share_elem.text.replace("Posted in: ","") + "]" + " @" + price_elem.text.replace("Price: ","") + Fore.RESET):26} '
-              f'{("(" + date_elem.text + ")"):20} '
-              f'{Fore.CYAN}{title_elem.text.replace(date_elem.text, "")}{Fore.RESET}')
-        print(f'{text_elem.text.strip()}')
+        print(
+            f'{(Fore.GREEN + name_elem.text + Fore.BLUE + " [" + share_elem.text.replace("Posted in: ","") + "]" + " @" + price_elem.text.replace("Price: ","") + Fore.RESET):26} '
+            f'{("(" + date_elem.text + ")"):20} '
+            f'{Fore.CYAN}{title_elem.text.replace(date_elem.text, "")}{Fore.RESET}'
+        )
+        print(f"{text_elem.text.strip()}")
         print()
 
         posts_retrieved += 1
@@ -99,7 +84,7 @@ for page_num in range(1, GET_MAX):
         sys.exit(1)
 
     if next_page_elem is None:
-        print(f'{Fore.RED}[!] No more pages found?{Fore.RESET}')
+        print(f"{Fore.RED}[!] No more pages found?{Fore.RESET}")
         sys.exit(1)
 
-print(f'{Fore.RED}[!] Exceeded GET_MAX({GET_MAX}) pages.{Fore.RESET}')
+print(f"{Fore.RED}[!] Exceeded GET_MAX({GET_MAX}) pages.{Fore.RESET}")
